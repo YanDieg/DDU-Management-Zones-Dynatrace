0eda8cbbca81571fe8181ef5db8f93cacd8c9b07
Yanez Diego Parolin <yanezprl@gmail.com>
2022-05-05 15:33:40 +0200

    add serveless and traces support

--- configuration.py
+++ configuration.py
@@ -8,13 +8,10 @@ API_TOKEN = 'PUTYOURTOKEN'
 MAX_REQUESTS_PER_MINUTE = 1000
 
 
-#PUT YOUR PERIOD HERE IN THIS FORMAT YYYY-MM-DD:HH:MM:SS+hh:mm where the hh:mm indicated the selected timezone (in italy 02:00)
 periods = [
-    ("2022-04-14T00:00:00+02:00", "2022-04-19T00:00:00+02:00"),
-    ("2022-04-19T00:00:00+02:00", "2022-04-23T00:00:00+02:00"),
+    ("2022-04-28T00:00:00+02:00", "2022-05-05T00:00:00+02:00"),
 ]
 
-#LIST OF METRICS (i will add serveless in future), you can select if enable it or not
 ddu_metrics = {
 'log' : 
     { "metric" : "builtin:billing.ddu.log.byEntity",
@@ -24,24 +21,35 @@ ddu_metrics = {
       "enable" : True},
 'events' : 
     {"metric" : "builtin:billing.ddu.events.byEntity",
-      "enable" : True},
+      "enable" : False},
+'traces' : 
+    {"metric" : "builtin:billing.ddu.traces.byEntity",
+      "enable" : False},
+'serveless' : 
+    {"metric" : "builtin:billing.ddu.serveless.byEntity",
+      "enable" : False},
 }
 
-#IF IT IS EMPTY THE SCRIPT WILL FIND AUTOMATICALLY THE MZs, IF YOU WANT A SPECIF MZ, PUT THE EXACT NAME IN THE LIST LIKE ["TEST1", "TEST2"]
 MZs = {
 'log' : [],
 'metrics' : [],
 'events' : [],
+'traces' : [],
+'serveless' : [],
 }
-#SAME AS FOR MZs BUT FOR ENTITIES, INSERT ENTITIES ONLY IF YOU ALREADY KNOW IT (NOT SUGGESTED)
+
 entities = {
 'log' : [],
 'metrics' : [],
 'events' : [],
+'traces' : [],
+'serveless' : [],
 }
 
+
+
+
 ################################
-#THIS LIST ARE THE ENTITIES THAT HAVEN?T THE MZs PROPERTIES; SO YOU CAN'T FILTER 
 not_good_entity = ["APM_SECURITY_GATEWAY", "APPLICATION_METHOD_GROUP", "AZURE_REGION", "BOSH_DEPLOYMENT", "BROWSER", 
 "CINDER_VOLUME", "CONTAINER_GROUP", "DCRUM_SERVICE_INSTANCE", "DEVICE_APPLICATION_METHOD_GROUP", 
 "DOCKER_CONTAINER_GROUP", "ENVIRONMENT", "GCP_ZONE", "GEOLOCATION", "GEOLOC_SITE", "HYPERVISOR_CLUSTER", 
